d85638cf6d41a23bbcfec7913d24988aa8e75cc2
Seeker04 <38542080+Seeker04@users.noreply.github.com>
2018-08-14 22:58:21 +0200

    Added usage options to description + minor changes
    Usage options were added to the description at the beginning of the file
    The results.txt file is now created as a hidden file
    New macros added for better readability
    MILLSEC is now unsigned int instead of unsigned long long as its big enough (can store more than a thousand hours)

--- main.c
+++ main.c
@@ -2,133 +2,132 @@
  * CUBE-TIMER
  *
  * A simple and lightweight program to record, store, list and get statistics
- * about Rubik's Cube times.
+ * of Rubik's Cube times
  *
+ * Options:
+ *          -a  Add a new time
+ *          -c  Clear results
+ *          -l  List all results
+ *          -s  Write statistics
  */
 
-#include <stdio.h>     /* FILE           */
-#include <stdlib.h>    /* exit()         */
-#include <sys/time.h>  /* gettimeofday() */
-#include <unistd.h>    /* sleep()        */
+#include <stdio.h>     /* printf, fgets, getchar, FILE */
+#include <stdlib.h>    /* exit                         */
+#include <sys/time.h>  /* gettimeofday                 */
+#include <unistd.h>    /* sleep                        */
 
-#define BUFF_SIZE 255
-#define FILE_NAME "results.txt"
-#define INSP_TIME 15
+#define CHAR_SHIFT 48 
+#define FILE_NAME  ".results.txt"
+#define INSP_TIME  15
+#define LINE_SIZE  11 
 
-typedef unsigned long long MILLSEC;
+typedef unsigned int MILLSEC;
 
-void    add      ();
-void    clear    ();
-MILLSEC deformat (char *const);
-void    format   (MILLSEC, char*);
-MILLSEC get_time ();
-void    list     ();
-void	stat     ();
+static void    add      ();
+static void    clear    ();
+static MILLSEC deformat (char *const);
+static void    format   (MILLSEC, char*);
+static MILLSEC get_time ();
+static void    list     ();
+static void    stat     ();
 
-FILE *f;
+static FILE *f;
 
-void
+static void
 add()
 {
-	MILLSEC st;  /* start time  */
-	MILLSEC ft;  /* finish time */
-	char res[10];
+	MILLSEC st;  /* start time */
+	char res[LINE_SIZE-1];
 
 	printf("Inspection time (%d secs)\n",INSP_TIME);
 	sleep(INSP_TIME);
 	st = get_time();
 	printf("Started\n");
 	getchar();
-	ft = get_time();
-	format(ft-st,res);
+	format(get_time()-st,res);
 	printf("Result: %s\n",res);
-	f = fopen(FILE_NAME,"a");
-	fprintf(f,"%s\n",res);
+	fprintf(f = fopen(FILE_NAME,"a"),"%s\n",res);
 	fclose(f);
 }
 
-void
+static void
 clear()
 {
-	f = fopen(FILE_NAME,"w");
-	fclose(f);
+	fclose(f = fopen(FILE_NAME,"w"));
 }
 
-void
+static void
 format(MILLSEC t, char *str)
 {
-	int min;
-	int sec;
-	int frac;
+	int sec  = t/1000;
+	int frac = t%1000;
+	int min  = sec/60;
 
-	sec = t/1000;
-	min = sec/60;
 	sec %= 60;
-	frac = t%1000;
 
-	str[0] = min<10 ? '0' : min/10;
-	str[1] = min<10 ? min+48 : min%10+48;
+	str[0] = min<10 ? '0' : min/10+CHAR_SHIFT;
+	str[1] = min<10 ? min+CHAR_SHIFT : min%10+CHAR_SHIFT;
 	str[2] = ':';
-	str[3] = sec<10 ? '0' : sec/10+48;
-	str[4] = sec<10 ? sec+48 : sec%10+48;
+	str[3] = sec<10 ? '0' : sec/10+CHAR_SHIFT;
+	str[4] = sec<10 ? sec+CHAR_SHIFT : sec%10+CHAR_SHIFT;
 	str[5] = '.';
-	str[6] = frac/100 ? frac/100+48 : '0';
-	str[7] = frac%100/10 ? frac%100/10+48 : '0';
-	str[8] = frac%10 ? frac%10+48 : '0';
+	str[6] = frac/100 ? frac/100+CHAR_SHIFT : '0';
+	str[7] = frac%100/10 ? frac%100/10+CHAR_SHIFT : '0';
+	str[8] = frac%10 ? frac%10+CHAR_SHIFT : '0';
 	str[9] = '\0';
 }
 
-MILLSEC
+static MILLSEC
 deformat(char *const str)
 {
 	MILLSEC time = 0;
 
-	time += (str[0]-48) * 600000;
-	time += (str[1]-48) * 60000;
-	time += (str[3]-48) * 10000;
-	time += (str[4]-48) * 1000;
-	time += (str[6]-48) * 100;
-	time += (str[7]-48) * 10;
-	time += (str[8]-48);
+	time += (str[0]-CHAR_SHIFT) * 600000;
+	time += (str[1]-CHAR_SHIFT) * 60000;
+	time += (str[3]-CHAR_SHIFT) * 10000;
+	time += (str[4]-CHAR_SHIFT) * 1000;
+	time += (str[6]-CHAR_SHIFT) * 100;
+	time += (str[7]-CHAR_SHIFT) * 10;
+	time += (str[8]-CHAR_SHIFT);
 
 	return time;
 }
 
 
-MILLSEC
+static MILLSEC
 get_time()
 {
 	struct timeval tp;
 
-	gettimeofday(&tp, NULL);
+	gettimeofday(&tp,NULL);
 	return (MILLSEC) tp.tv_sec * 1000L + tp.tv_usec / 1000; 
 }
 
-void
+static void
 list()
 {
-	char line[BUFF_SIZE];
+	char line[LINE_SIZE];
 	int  i = 0;
 
 	f = fopen(FILE_NAME,"r");
-	while (fgets(line,BUFF_SIZE,f))
+	while (fgets(line,LINE_SIZE,f))
 		printf("%d. %s",++i,line);
 	fclose(f);
 }
 
-void
+static void
 stat()
 {
 	/* TODO: Add "Average of 5" and "Average of 12" */
 
-	char    line[BUFF_SIZE];
+	char    line[LINE_SIZE];
 	MILLSEC time;
-	MILLSEC best  = 0;
 	MILLSEC sum   = 0;
+	MILLSEC best  = 0;
 	int     count = 0;
 
 	f = fopen(FILE_NAME,"r");
-	while (fgets(line,BUFF_SIZE,f)) {
+	while (fgets(line,LINE_SIZE,f)) {
 		time = deformat(line);
 		best = time<best || !best ? time : best;
 		sum += time;
@@ -164,7 +163,6 @@ main(int argc, char const *argv[])
 	default  : printf("Error: Illegal argument!\n");
 		   exit(2);
 	}
-
 	return 0;
 }
 
